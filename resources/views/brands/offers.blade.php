fa2c1153c0b4c021abd519f9029ec9e6c2ec6830
Cotto213 <alfonsocotto5b12@gmail.com>
2023-02-18 14:41:41 -0600

    Initial commit

---
+++ resources/views/brands/offers.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'offers')
+
+@section('content')
+
+    <div class="text-danger font-bold text-xl">This section shows current offers!</div>
+    @include('utilities.offers')
+
+@endsection
